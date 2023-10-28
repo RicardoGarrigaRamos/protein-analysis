0ba3bdd543315fa0185fc6e711eb73f374e0c4cc
RicardoGarrigaRamos <ricardogarriga10@gmail.com>
2023-10-28 11:03:08 -0400

    Deriving Attributes through Rapid miner
    Short guide on how were gonna integrate python into rapid miner to derive attributes

---
+++ DeriveAttributes.py
@@ -0,0 +1,17 @@
+import pandas
+
+def rm_main(df):
+
+    # your rapid miner might not have the execute python operator see
+    # https://docs.rapidminer.com/latest/developers/python/extension/
+
+
+
+    # here is wear we would call our functions
+
+
+
+
+    
+
+    return df
